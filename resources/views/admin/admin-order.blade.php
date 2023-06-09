98d7f2c266ec3ba28a1cbe18775d5243fcf58523
ahmdf20 <ahmadfadilah200603@gmail.com>
2023-06-09 08:54:21 +0700

    fix

--- resources/views/admin/admin-order.blade.php
+++ resources/views/admin/admin-order.blade.php
@@ -13,27 +13,15 @@
             <div class="col-lg mb-lg-0 mb-4">
                 <div class="card z-index-2 mb-4">
                     <div class="card-header pb-0">
-                        <a href="{{ route('customer.create') }}" class="btn btn-sm btn-success float-end">Tambah Data</a>
                         <h6>Tabel Pesanan</h6>
                     </div>
                     <div class="card-body px-0 pt-0 pb-2">
-                        <div class="row justify-content-end mx-2">
-                            <div class="col-lg-4 col-md-6 col-sm-4">
-                                <form action="" method="get">
-                                    <div class="input-group">
-                                        <input type="text" name="search" class="form-control">
-                                        <button class="input-group-text bg-success text-white" type="submit">Cari</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
                         <div class="table-responsive p-0">
                             <table class="table align-items-center mb-0">
                                 <thead>
                                     <tr>
                                         <th class="text-center">#</th>
-                                        <th
-                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
                                             Nama</th>
                                         <th
                                             class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
